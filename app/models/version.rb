24cf839240baf13688df79be1970aa12fc1cc717
Edwin & Shawn Wilkinson <pair+Dwin357.shawndav@devbootcamp.com>
2015-08-03 15:37:54 -0500

    connected all models and fleshed out seed

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,7 +1,7 @@
 class Version < ActiveRecord::Base
   belongs_to :article
   # No user model yet...
-  # belongs_to :updating_author, class_name: :"User"
+  belongs_to :updating_author, class_name: :"User"
   has_many :categorizations
   has_many :categories, through: :categorizations
 end
