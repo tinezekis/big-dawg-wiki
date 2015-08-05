9ad36bc257664e782d6de652d6c2b56a6c22e40f
Mark Donald Amobi <markdnd007@gmail.com>
2015-08-05 14:29:51 -0500

    fix a merge conflict in form

--- db/article_seeds.rb
+++ db/article_seeds.rb
@@ -3,25 +3,25 @@ Article.destroy_all if Article.any?
 Version.destroy_all if Version.any?
 Categorization.destroy_all if Categorization.any?
 
-users = User.all
+# users = User.all
 
 
 
-['dog', 'cat', 'chicken', 'rooster', 'rocketship', 'solar-yatch', 'space-poodle', 'small-rodent', 'cat-people', 'roller-coaster'].each do |word| 
-  Category.create!(name: word)
-end
-cat = Category.all
+# ['dog', 'cat', 'chicken', 'rooster', 'rocketship', 'solar-yatch', 'space-poodle', 'small-rodent', 'cat-people', 'roller-coaster'].each do |word|
+#   Category.create!(name: word)
+# end
+# cat = Category.all
 
-10.times do
-  a = Article.create!(orig_author: users.sample,
-                      title: Faker::Commerce.color)
-  (rand(5)+1).times do
-  v = Version.create!(updating_author: users.sample,
-                  article: a,
-                  content: Faker::Company.bs,
-                  footnotes: Faker::Company.catch_phrase)
+# 10.times do
+#   a = Article.create!(orig_author: users.sample,
+#                       title: Faker::Commerce.color)
+#   (rand(5)+1).times do
+#   v = Version.create!(updating_author: users.sample,
+#                   article: a,
+#                   content: Faker::Company.bs,
+#                   footnotes: Faker::Company.catch_phrase)
 
-  Categorization.create!(version: v,
-                         category: cat.sample)
-  end
-end
+#   Categorization.create!(version: v,
+#                          category: cat.sample)
+#   end
+# end
